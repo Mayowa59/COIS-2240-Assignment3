0c74856ed8a07a99d18f057295dc62e98fe89769
Mayowa Adeyeri <mayowaadeyeri@trentu.ca>
2026-03-29 12:23:30 -0400

    fixed the error

--- Vehicle.java
+++ Vehicle.java
@@ -13,8 +13,16 @@ public abstract class Vehicle {
         this.year = year;
         this.status = VehicleStatus.Available;
         this.licensePlate = null;
+        
     }
-
+   
+    private String capitalize(String input) {
+        if (input == null || input.isEmpty()) {
+            return null;
+        }
+        return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
+    }
+    
     public Vehicle() {
         this(null, null, 0);
     }
